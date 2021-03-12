ac452e460a3b69c1f33548733069ab99d49f2245
朱凯 <zhukai@homolo.com>
2021-03-12 09:41:35 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router";
-import Novice from "novice-boot-vue/src/build";
+import Novice from "novice-boot-vue";
 
 createApp(App).use(Novice).use(router).mount('#app');
